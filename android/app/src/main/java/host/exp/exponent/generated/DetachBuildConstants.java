78e3b9f3ce2c4f57661021fd8697d2499ee84dda
Adrian Carolli <adrian.carolli1@gmail.com>
2018-06-05 11:01:38 -0400

    expo detached instabug app

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp5ce40905eeb747e29f25deb0ffb0916d://x8-sk2.flyreel.expo-instabug-sample.exp.direct:80";
+
+}
